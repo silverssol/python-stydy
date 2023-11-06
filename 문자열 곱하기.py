bce9a2c70bf7050c415826847e0608f9a1cf0231
eunsol <esol04cc@gmail.com>
2023-11-06 17:46:30 +0900

    문자열로 변환

---
+++ 문자열 곱하기.py
@@ -0,0 +1,5 @@
+# /school.programmers.co.kr/learn/courses/30/lessons/181940
+
+def solution(my_string, k):
+    answer = my_string * k
+    return answer
---
+++ 문자열로 변환.py
@@ -0,0 +1,5 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181845
+
+def solution(n):
+    answer = str(n)
+    return answer
